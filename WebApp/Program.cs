6b607d22be87b14269bbdde577181d7f2721ea56
Martijn <martijn@kaskon.it>
2023-11-13 12:56:47 +0100

    Delete weatherforecast

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -10,7 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 builder.Services.AddApplication();
 builder.Services.AddPersistence(builder.Configuration);
